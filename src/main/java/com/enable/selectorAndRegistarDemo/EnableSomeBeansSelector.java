a4147f27b7eaab9a183ff5c2a57d78e58a40f3ba
Mengleifeng <fmleing@163.com>
2020-01-04 10:55:00 +0800

    组合Selector 和 Registar 方式

---
+++ src/main/java/com/enable/selectorAndRegistarDemo/EnableSomeBeansSelector.java
@@ -0,0 +1,16 @@
+package com.enable.selectorAndRegistarDemo;
+
+import com.enable.selectorDemo.SomeBeanConfigurationSelector;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Import(SomeBeanConfigurationSelector.class)
+public @interface EnableSomeBeansSelector {
+    String criteria() default "default";
+}
